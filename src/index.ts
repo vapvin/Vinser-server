8adfb2b04df224ffd60272e77bda54629d21676b
vapvin <tpehdj12@daum.net>
2020-01-27 02:45:23 +0900

    Settings Server, GraphQL yoga and Express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello World');
+import { Options } from 'graphql-yoga';
+import app from './app';
+
+const PORT: number | string = process.env.PORT || 3333;
+const PLAYGROUND: string = '/playground';
+const GRAPHQL_ENDPOINT: string = '/graphql';
+
+const appOptions: Options = {
+  port: PORT,
+  playground: PLAYGROUND,
+  endpoint: GRAPHQL_ENDPOINT
+};
+
+const handleAppStart = () => console.log(`Start Server on localhost:${PORT}`);
+
+app.start(appOptions, handleAppStart);
