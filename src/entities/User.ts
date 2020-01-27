8abaff32892a93352ab3091b848e5579bb92e943
vapvin <tpehdj12@daum.net>
2020-01-27 13:07:05 +0900

    User Entity 02 Complete

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -40,6 +40,24 @@ class User extends BaseEntity {
   @Column({ type: 'text' })
   profilePhoto: string;
 
+  @Column({ type: 'boolean', default: false })
+  isDriving: boolean;
+  @Column({ type: 'boolean', default: false })
+  idRiding: boolean;
+  @Column({ type: 'boolean', default: false })
+  isTaken: boolean;
+
+  @Column({ type: 'double precision', default: 0 })
+  lastLng: number;
+  @Column({ type: 'double precision', default: 0 })
+  lastLat: number;
+  @Column({ type: 'double precision', default: 0 })
+  lastOrientation: number;
+
+  get fullName(): string {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
   @CreateDateColumn() createdAt: string;
   @UpdateDateColumn() updatedAt: string;
 }
