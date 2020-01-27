dfdc139ea4322b2d83278e99f0a7ded7d706feb7
vapvin <tpehdj12@daum.net>
2020-01-27 13:21:10 +0900

    Hashing and Encrypting

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,12 +1,17 @@
+import bcrypt from 'bcrypt';
 import { IsEmail } from 'class-validator';
 import {
   BaseEntity,
   Column,
+  BeforeInsert,
+  BeforeUpdate,
   CreateDateColumn,
   Entity,
   PrimaryGeneratedColumn,
   UpdateDateColumn
 } from 'typeorm';
+
+const BCRYPT_ROUND = 10;
 
 @Entity()
 class User extends BaseEntity {
@@ -54,12 +59,25 @@ class User extends BaseEntity {
   @Column({ type: 'double precision', default: 0 })
   lastOrientation: number;
 
+  @CreateDateColumn() createdAt: string;
+  @UpdateDateColumn() updatedAt: string;
+
   get fullName(): string {
     return `${this.firstName} ${this.lastName}`;
   }
 
-  @CreateDateColumn() createdAt: string;
-  @UpdateDateColumn() updatedAt: string;
+  @BeforeInsert()
+  @BeforeUpdate()
+  async savePassword(): Promise<void> {
+    if (this.password) {
+      const hashedPassword = await this.hashPassword(this.password);
+      this.password = hashedPassword;
+    }
+  }
+
+  private hashPassword(password: string): Promise<string> {
+    return bcrypt.hash(password, BCRYPT_ROUND);
+  }
 }
 
 export default User;
