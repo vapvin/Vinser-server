b9dcd584f696ba3f7ecc0274f7301ba30037ba29
vapvin <tpehdj12@daum.net>
2020-02-11 20:02:21 +0900

    Custom Auth Middleware

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import { GraphQLServer } from 'graphql-yoga';
 import helmet from 'helmet';
 import loger from 'morgan';
 import schema from './schema';
+import decodeJWT from './utils/decodeJWT';
 
 class App {
   public app: GraphQLServer;
@@ -16,10 +17,16 @@ class App {
     this.app.express.use(cors());
     this.app.express.use(loger('dev'));
     this.app.express.use(helmet());
+    this.app.express.use(this.jwt);
   };
 
   private jwt = async (req, res, next): Promise<void> => {
     const token = req.get('X-JWT');
+    if (token) {
+      const user = await decodeJWT(token);
+      console.log(user);
+    }
+    next();
   };
 }
 
